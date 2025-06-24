1951de50a12d957ffe6bf7b843ea7f77779addd9
Hadrien LOUICHON <hadrien.louichon@imt-atlantique.net>
2025-06-24 10:00:10 +0300

    code : improvement of the algo

--- ML/MLM_recursive.py
+++ ML/MLM_recursive.py
@@ -10,69 +10,46 @@ import seaborn as sns
 # --- Load Salinas-A data ---
 def load_salinas_A():
     data = scipy.io.loadmat('ML/SalinasA_corrected.mat')['salinasA_corrected']
-    #print(data)
-    print("Shape of Salinas-A data:", data.shape)
     labels = scipy.io.loadmat('ML/SalinasA_gt.mat')['salinasA_gt']
-    #print(labels)
-    print("Shape of Salinas-A labels:", labels.shape)
-    
-    # Remove the background : class (0)
     mask_background = labels > 0
     data[~mask_background], labels[~mask_background] = 0, 0
-    
     return data, labels
 
 # Selection of the reference points (3 per class, randomly selected)
-def select_random_points(data, labels):
-    #classes = np.unique(training_labels[training_labels > 0])
+def select_random_points(data, labels, n):
     classes = np.unique(labels)
-    #print(classes)
-    #print(type(classes))
-    R = [] # Reference points, 3 per classes selected randomly
-    T = [] # Labels of reference points
+    R = []
+    T = []
 
     for label in classes:
         class_indices = np.where(labels == label)[0]
-        if len(class_indices) < 3:
+        if len(class_indices) < n:
             raise ValueError(f"Not enough points for the class {label}.")
-        chosen_indices = np.random.choice(class_indices, size=3, replace=False)
+        chosen_indices = np.random.choice(class_indices, size=n, replace=False)
         print("Points for class :", label, "are : ", chosen_indices)
         R.extend(data[chosen_indices])
-        T.extend([label, label, label])
-    
-    print("Number of reference points selected:", len(R))
+        T.extend([label]*n)
+    print("Number of points selected:", len(R))
     return np.array(R), np.array(T)
 
 # Reshape the data for distance calculations :
 def reshape_data(data, labels):
-    data = data.reshape(-1, data.shape[-1])  # Flatten the last dimension
-    labels = labels.reshape(-1)  # Flatten the labels
-    
-    #print("Reshaped data shape:", data.shape)
-    #print("Reshaped labels shape:", labels.shape)
+    data = data.reshape(-1, data.shape[-1])
+    labels = labels.reshape(-1)
     return data, labels
 
 # Build the distances matrices :
 def build_distance_matrices(data, R, labels, T):
     labels = labels.astype(float)  
     T = T.astype(float)
-
-    #Distance_out = euclidean_distances(labels[:, np.newaxis], T[:, np.newaxis])
-    #Distance_in = euclidean_distances(data, R)
     Distance_out = np.array([[np.linalg.norm(l - ref_label) for ref_label in T] for l in labels])
     Distance_in = np.array([[np.linalg.norm(d - ref_data) for ref_data in R] for d in data])
-    
-    #print("Distance_out shape:", Distance_out.shape)
-    #print("Distance_in shape:", Distance_in.shape)
     return Distance_out, Distance_in
 
 # Initialization 
 def rls_initialization(Distance_out, Distance_in):
     P0 = np.linalg.pinv(Distance_in.T @ Distance_in)
     B0 = P0 @ Distance_in.T @ Distance_out
-
-    #print("P0 shape:", P0.shape)
-    #print("B0 shape:", B0.shape)
     return P0, B0
 
 # Update the RLS model
@@ -81,14 +58,10 @@ def rls_update(New_distance_out, New_distance_in, P, B):
     P_new = P - P_x @ np.linalg.inv(np.eye(New_distance_in.shape[0]) + New_distance_in @ P_x) @ New_distance_in @ P
     K_i = P_new @ New_distance_in.T
     B_new = B + K_i @ (New_distance_out - New_distance_in @ B)
-    
-    #print("P_new shape:", P_new.shape)
-    #print("B_new shape:", B_new.shape)
     return P_new, B_new
 
 # Predict the label for new data points
 def predict_label(X_new, R, T, B):
-    #Dx_new = euclidean_distances(X_new, R)
     Dx_new = np.array([[np.linalg.norm(x - ref_data) for ref_data in R] for x in X_new])
     predictions = Dx_new @ B
     predicted_labels = np.argmin(predictions, axis=1)
@@ -105,7 +78,7 @@ def plot_confusion_matrix(y_true, y_pred):
     plt.ylabel("Actual")
     plt.show()
 
-# --- Comparison maps with color to differiate the labels ---
+# Comparison maps with color to differiate the labels
 def plot_comparison_maps(true_labels, pred_labels, shape, class_names, class_colors, class_values):
     true_reshaped = true_labels.reshape(shape)
     pred_reshaped = pred_labels.reshape(shape)
@@ -148,10 +121,10 @@ if __name__ == "__main__":
     data, labels = reshape_data(testing_data, testing_labels)
    
     # Select random reference points
-    R, T = select_random_points(data, labels)
+    R, T = select_random_points(data, labels, n=3)
     # Select random training data
-    training_data, training_labels = select_random_points(data, labels)
-    print(training_data, training_labels)
+    training_data, training_labels = select_random_points(data, labels, n=3)
+    #print(training_data, training_labels)
     
     # Build the model from the training data
     Distance_out, Distance_in = build_distance_matrices(training_data, R, training_labels, T)
@@ -164,20 +137,17 @@ if __name__ == "__main__":
         print("Processing row:", i+1)
         data = testing_data[i,:]
         label = testing_labels[i,:]
+        
         for j in range(testing_data.shape[1]):
             pixel, pix_label = reshape_data(data[j], label[j])
-            # Make predictions for the current data point
             predicted_label = predict_label(pixel, R, T, B)
             predictions_array[i,j] = predicted_label
-            #print("Label to predict:", pix_label)
-            #print("Predicted label for pixel:", predicted_label)
-            # Update the RLS model with new data
+
         valid_pixels = label != 0
-        #print(valid_pixels)
         data_valid = data[valid_pixels]
         label_valid = label[valid_pixels]
+        
         New_distance_out, New_distance_in = build_distance_matrices(data_valid, R, label_valid, T)
-        #New_distance_out, New_distance_in = build_distance_matrices(data, R, label, T)
         P, B = rls_update(New_distance_out, New_distance_in, P, B)
 
     print(accuracy_score(testing_labels.reshape(-1), predictions_array.reshape(-1)))
